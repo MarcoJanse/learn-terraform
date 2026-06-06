efa4c0269fce999a2cc11e57d02931c83ff6d147
Marco Janse <21693491+MarcoJanse@users.noreply.github.com>
2026-06-06 22:08:11 +0200

    feat: add az change tutorial

--- tf-associate/get-started-azure/main.tf
+++ tf-associate/get-started-azure/main.tf
@@ -17,4 +17,16 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "rg-learn-tf-tst"
   location = "northeurope"
+
+  tags = {
+    Environment = "Test"
+    Team = "DevOps"
+  }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name = "myTFVnet"
+  address_space = [ "10.0.0.0/16" ]
+  location = "northeurope"
+  resource_group_name = azurerm_resource_group.rg.name
 }
